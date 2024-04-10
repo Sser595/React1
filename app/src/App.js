21916f187391c1ad3ab7dcc943ab568770febbba
lai2-24 <lai2-24@itismeucci.com>
2024-04-10 13:33:14 +0200

    added Ciccio

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Ciccio from './Ciccio.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Ciccio nome="Serena Valentini" eta="20"/>
+    </div>
+  );
+}
+
+export default App;
